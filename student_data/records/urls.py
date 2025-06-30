59b32e595d996bdff919ceef5291ff26c6a76c61
Ankit-euphemism <153090276+Ankit-euphemism@users.noreply.github.com>
2025-06-30 11:32:21 +0530

    Update urls.py

--- student_data/records/urls.py
+++ student_data/records/urls.py
@@ -4,5 +4,5 @@ from .views import mark_attendance, view_attendance
 urlpatterns = [
 #    path('admin/', admin, name='admin')
     path('mark/', mark_attendance, name='mark_attendance'),
-    path('view/', view_attendance, name='view_attendance'),
+    path('', view_attendance, name='view_attendance'),
 ]
